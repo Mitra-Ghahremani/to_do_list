52b63c39b8bfc78ec7c3f5d638f66104a7519613
Mitra-Ghahremani <mitrayamgh1996@gmail.com>
2025-07-06 00:00:44 +0430

    added category model

--- todolist_app/models.py
+++ todolist_app/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 
-
-
+class Category(models.Model):
+    name=models.CharField(max_length=20)
+    def __str__(self):
+        return self.name
+    
 class Task(models.Model):
-    title=models.CharField(max_length=20,null=True,blank=True)
+    category=models.ForeignKey(Category,on_delete=models.CASCADE)
+    title=models.CharField(max_length=20)
     description=models.TextField(max_length=150,null=True,blank=True)
     datetime_start=models.DateTimeField(null=True,blank=True)
     datetime_end=models.DateTimeField(null=True,blank=True)
     situation=models.BooleanField(default=False)
+    
+
+    def __str__(self):
+        return self.title
--- todolist_app/views.py
+++ todolist_app/views.py
@@ -1,29 +1,28 @@
 from django.shortcuts import render,redirect
-from . models import Task
-from datetime import timedelta,datetime
+from . models import Task,Category
+from datetime import datetime
 from django.core.paginator import Paginator
 
 def add_task(request):
     if request.method=="POST":
+        category=request.POST.get("category")
         title=request.POST.get("title")
-        description=request.POST.get("description")
-        obj=request.POST.get("id")
-        object=Task.objects.all()
-        for item in object:
-            if item.id ==obj:
-              Task.save(title=title,description=description)
-        Task.objects.create(title=title,description=description)
-      
+        description=request.POST.get("description")  
+        Task.objects.create(title=title,description=description,category_id=category)
         return redirect("/plan")
-    return render(request,"todolist_app/add.html",{})
+    category_object=Category.objects.all()
+    return render(request,"todolist_app/add.html",{"categories":category_object})
+
+
 
 
 def list_task_not_done(request):
     list_task=Task.objects.filter(situation=False).order_by('-id') 
-    page=request.GET.get('page')
     paginator=Paginator(list_task,4)
+    page=request.GET.get('page')
     page_obj=paginator.get_page(page)
     return render(request,"todolist_app/do_not.html",context={"page_obj":page_obj})
+
 def delete(request,task_id):
     delete_object=Task.objects.get(id=task_id)
     delete_object.delete()
@@ -32,19 +31,28 @@ def delete(request,task_id):
 
 def edit(request,task_id):
     if request.method=="POST":
+          category=request.POST.get("category")
           title=request.POST.get("title")
           description=request.POST.get("description")
           object=Task.objects.get(id=task_id)
           object.title=title
           object.description=description
+          object.category_id=category
           object.save()
           return redirect("/plan")
 
 
     object=Task.objects.get(id=task_id)
+    #category=object.category_id.name
+    #category=object.category.name
+    #category=object.category.name
+    #category_id=object.category.id
+    category=object.category.id
+
     title=object.title
     description=object.description
-    return render (request,"todolist_app/edit.html",{"title":title,'description':description})
+    categories=Category.objects.all()
+    return render (request,"todolist_app/edit.html",{"title":title,'description':description,"category_id":category,"categories":categories})
 
 
 def start_task(request,task_id):
@@ -73,8 +81,6 @@ def complete_task(request,task_id):
     page_obj=paginator.get_page(page)
     return render(request,"todolist_app/do_not.html",context={"page_obj":page_obj})
     
-
-
 def list_task_done(request):
     durations=[]
     objects=Task.objects.filter(situation=True).order_by("-id")
@@ -93,7 +99,7 @@ def list_task_done(request):
     objects=list(zip(objects,durations))
     paginator=Paginator(objects,3)
     page_obj=paginator.get_page(page)
-    return render(request,"todolist_app/complete.html",context={"page_obj":page_obj})
+    return render(request,"todolist_app/done.html",context={"page_obj":page_obj})
 
 
     
